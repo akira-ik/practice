8e6586b60f8e1d97fdc265f95342b35f68711e16
akira-ik <147118904+akira-ik@users.noreply.github.com>
2023-10-10 10:26:46 +0900

    Create TngibleAsset.java

---
+++ TngibleAsset.java
@@ -0,0 +1,19 @@
+public abstract class TngibleAsset{
+  private String name;
+  private int price;
+  private String color;
+  public TngibleAsset(Stirng neme, int price, String color){
+    this.name = name;
+    this.price = price;
+    this.coloe = coler;
+  }
+  public String getNmae(){
+    return this.name;
+  }
+  public int getPrice(){
+    return this.price;
+  }
+  public Stirng getColor(){
+    return this.color;
+  }
+  
